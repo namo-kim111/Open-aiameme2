0d88506692cab087c44b740b0182a8f3fdf8bb83
namo-kim111 <euna8070@gmail.com>
2025-07-15 22:35:36 +0900

    Update App1.py

--- App1.py
+++ App1.py
@@ -15,7 +15,7 @@ def build_prompt(meme_name):
 """
 
 def query_huggingface_model(prompt):
-    api_url = "https://api-inference.huggingface.co/models/google/flan-t5-base"
+    api_url = "https://api-inference.huggingface.co/models/bigscience/bloom"
     response = requests.post(api_url, headers=headers, json={"inputs": prompt})
     if response.status_code == 200:
         result = response.json()
